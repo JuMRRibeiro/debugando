2e273fd4f04ba06269d1c676a1d61e53e19d1da2
JuMRRibeiro <julianathmartins@gmail.com>
2019-11-12 22:13:05 -0300

    debugando

---
+++ src/model/alunas.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const AlunasSchema = new mongoose.Schema({
+    nome: { type: String },
+    dateOfBirth: { type: String },
+    nasceuEmSp: { type: String },
+    livros: [{
+        titulo: String,
+        leu: String,
+    }]
+})
+
+/*var sampleSchema = new Schema ({ nome: { type: String, required: true}});*/
+
+const Alunas = mongoose.model('Alunas', AlunasSchema)
+
+module.exports = Alunas
